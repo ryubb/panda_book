d877348ea609b315d453e4083cf3583359d409f6
ryubb <ryubb1108@gmail.com>
2019-08-26 13:15:05 +0900

    feat:レスポンスをファイル分割により実現

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+import root from "./test";
+
 var express = require("express");
 var app = express();
 
 app.set("port", process.env.PORT || 5000);
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(request, response) {
-  response.send("Hello World!");
+app.get("/", function(req, res) {
+  root(res);
 });
 
 app.listen(app.get("port"), function() {
