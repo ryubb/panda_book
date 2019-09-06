56e677624ec3303b69453735548487b9cc7da899
ryubb <ryubb1108@gmail.com>
2019-09-06 22:21:26 +0900

    feat:ログイン認証機能の作成

--- node/app.js
+++ node/app.js
@@ -1,9 +1,15 @@
+"user strict";
 const http = require("http");
 const express = require("express");
 const path = require("path");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 
+const User = require("./schema/User");
+const Book = require("./schema/Book");
 const Message = require("./schema/Message");
 
 const app = express();
@@ -18,10 +24,89 @@ mongoose.connect("mongodb://localhost:27017/chatapp", err => {
 
 app.use(bodyparser());
 
+app.use(session({ secret: "HogeFuga" }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// pugの設定
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 app.get("/", (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) throw err;
+    return res.render("index", {
+      users: users,
+      loginUser: req.session && req.session.user ? req.session.user : null
+    });
+  });
+});
+
+app.get("/messages", (req, res, next) => {
   Message.find({}, (err, msgs) => {
     if (err) throw err;
     return res.status(200).json({ msgs });
+  });
+});
+
+app.get("/signin", (req, res, next) => {
+  return res.render("signin");
+});
+
+app.post("/signin", (req, res, next) => {
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(err => {
+    if (err) throw err;
+    return res.redirect("/");
+  });
+});
+
+app.get("/login", (req, res, next) => {
+  return res.render("login");
+});
+
+app.post("/login", passport.authenticate("local"), (req, res, next) => {
+  User.findOne({ _id: req.session.passport.user }, (err, user) => {
+    if (err || !req.session) return res.redirect("/login");
+
+    req.session.user = {
+      username: user.username
+    };
+    return res.redirect("/");
+  });
+});
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      console.log("passport");
+      console.log(user);
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findOne({ _id: id }, (err, user) => {
+    done(err, user);
   });
 });
 
