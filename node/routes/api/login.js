e1d6f0aaf4ca905868076a5de232f7541155f82c
ryubb <ryubb1108@gmail.com>
2019-09-23 16:03:13 +0900

    fix:必要なファイルの追加

---
+++ node/routes/api/login.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+
+const User = require("../../schema/User");
+
+router.post("/signup", (req, res) => {
+  const hashed_password = bcrypt.hashSync(req.body.password, 10);
+  const newUser = new User({
+    email: req.body.email,
+    password: hashed_password
+  });
+  console.log(newUser);
+  newUser.save(err => {
+    if (err) throw err;
+    console.log("signup success!!");
+    return res.status(200);
+  });
+});
+
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err || !user) {
+      console.log("invalid email");
+      return res.status(500).json({ message: "存在しないメールアドレスです" });
+    }
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      console.log("invalid password");
+      return res.status(500).json({ message: "パスワードが間違ってます" });
+    }
+    jwt.sign({ user }, "pandabook", { expiresIn: 1000 }, (err, token) => {
+      if (err) {
+        console.log("fail to create token");
+        return res.status(500).json({ message: "tokenの生成に失敗しました" });
+      }
+      if (token) {
+        console.log("login success!");
+        return res.status(200).json({ token: token });
+      }
+    });
+  });
+});
+
+module.exports = router;
---
+++ node/routes/api/timeline.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const verifyToken = require("../../services/verify");
+
+const User = require("../../schema/User");
+const Timeline = require("../../schema/Timeline");
+
+router.get("/", verifyToken, function(req, res) {
+  Timeline.find({})
+    .populate("user")
+    .exec((err, result) => {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+router.post("/post", verifyToken, function(req, res) {
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.verify(token, "pandabook");
+
+  User.findOne({ _id: decoded.user._id }, (err, result) => {
+    if (err) throw err;
+    const timeline = new Timeline({
+      content: req.body.content,
+      user: result._id
+    });
+
+    timeline.save(err => {
+      if (err) throw err;
+      Timeline.findOne({ _id: timeline._id })
+        .populate("user")
+        .exec((err, result) => {
+          if (err) throw err;
+          console.log(result);
+          res.json(result);
+        });
+    });
+  });
+});
+
+module.exports = router;
